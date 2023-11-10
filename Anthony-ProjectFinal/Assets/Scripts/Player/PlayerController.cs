1755cae7bd8ee9862f4de38521b97a389fd4c886
Anthony Niculescu-Cornejo <117235442+Anicor147@users.noreply.github.com>
2023-11-10 12:36:09 -0500

    PlayerScript and Base Enemy
    The skeleton is created
    Created SO for stats

--- Anthony-ProjectFinal/Assets/Scripts/Player/PlayerController.cs
+++ Anthony-ProjectFinal/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Enemies;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private float _maxHealth;
+    private float _maxStamina;
+    private float _expCap;
+    private int _damage;
+    public float CurrentHealth { get; set; }
+    public float CurrenStamina{ get; set; }
+    public float CurrentExp { get; set; }
+    public int Damage { get; set; }
+
+    private void Start()
     {
-        
+        CurrentHealth = _maxHealth;
+        CurrenStamina = _maxStamina;
+    }
+    
+    public void OnDamage(float damage)
+    {
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDeath()
     {
-        
     }
+    
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<EnemyBase>().TakeDamage(Damage);
+        }
+    }
+    
 }
